e87043c8d28de49ef45879da540cdf1a06a7a5db
Luke Yuan <ly94@cornell.edu>
2021-11-18 11:44:30 -0500

    tag_test

---
+++ finalprogram/tag_test.py
@@ -0,0 +1,51 @@
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from pupil_apriltags import Detector
+import time
+import cv2
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+rawCapture = PiRGBArray(camera)
+# allow the camera to warmup
+time.sleep(0.1)
+# grab an image from the camera
+camera.capture(rawCapture, format="bgr")
+image = rawCapture.array
+# display the image on screen and wait for a keypress
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+print("[INFO] detecting AprilTags...")
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
+results = detector.detect(gray)
+print("[INFO] {} total AprilTags detected".format(len(results)))
+
+for r in results:
+	# extract the bounding box (x, y)-coordinates for the AprilTag
+	# and convert each of the (x, y)-coordinate pairs to integers
+	(ptA, ptB, ptC, ptD) = r.corners
+	ptB = (int(ptB[0]), int(ptB[1]))
+	ptC = (int(ptC[0]), int(ptC[1]))
+	ptD = (int(ptD[0]), int(ptD[1]))
+	ptA = (int(ptA[0]), int(ptA[1]))
+	# draw the bounding box of the AprilTag detection
+	cv2.line(image, ptA, ptB, (0, 255, 0), 2)
+	cv2.line(image, ptB, ptC, (0, 255, 0), 2)
+	cv2.line(image, ptC, ptD, (0, 255, 0), 2)
+	cv2.line(image, ptD, ptA, (0, 255, 0), 2)
+	# draw the center (x, y)-coordinates of the AprilTag
+	(cX, cY) = (int(r.center[0]), int(r.center[1]))
+	cv2.circle(image, (cX, cY), 5, (0, 0, 255), -1)
+	# draw the tag family on the image
+	tagFamily = r.tag_family.decode("utf-8")
+	cv2.putText(image, tagFamily, (ptA[0], ptA[1] - 15),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+	print("[INFO] tag family: {}".format(tagFamily))
+
+cv2.imwrite("detected.jpg", image)
